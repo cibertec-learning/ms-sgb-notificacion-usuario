785d4ddb692b69b364799424062e4f6c3a17d294
Fatima Victoria Urpi Chavez <143281970+FatimaUrpi@users.noreply.github.com>
2024-12-10 06:05:01 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/cibertec/dawii/ms/notificaciones/MsSgbNotificacionUsuarioApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.dawii.ms.notificaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSgbNotificacionUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSgbNotificacionUsuarioApplication.class, args);
+	}
+
+}
